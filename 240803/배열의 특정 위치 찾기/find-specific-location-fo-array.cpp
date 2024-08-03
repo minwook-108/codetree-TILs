be1b3555dcce566132dccae776b626bd53428721
minwook-108 <kimmw0321@naver.com>
2024-08-03 19:14:29 +0900

    Title: 배열의 특정 위치 찾기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240803/배열의 특정 위치 찾기/find-specific-location-fo-array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    int sum1=0, sum2=0, cnt=0;
+    for(int i=0;i<10;i++){
+        cin >> arr[i];
+        if(i%2==1){
+            sum1+=arr[i];
+        }
+        if((i+1)%3==0){
+            sum2+=arr[i];
+            cnt++;
+        }
+    }
+    cout << fixed;
+    cout.precision(1);
+    cout << sum1 << " " << (double)sum2/cnt;
+    return 0;
+}
