26b680fdeeeb9091dd3a5248331b37c14aff798e
minwook-108 <kimmw0321@naver.com>
2024-08-03 21:55:57 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240803/특정 조건에 맞게 출력하기/print-in-specific-conditions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100];
+    for(int i=0;i<100;i++){
+        cin >> arr[i];
+        if(arr[i]==0){
+            break;
+        }
+        if(arr[i]%2==1){
+            cout << arr[i]+3 << " ";
+        }
+        else{
+            cout << arr[i]/2 << " ";
+        }
+    }
+    return 0;
+}
