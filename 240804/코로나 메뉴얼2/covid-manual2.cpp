c7b9862e41a9561d3fd3bdd925a2db6035a0d649
minwook-108 <kimmw0321@naver.com>
2024-08-04 21:30:14 +0900

    Title: 코로나 메뉴얼2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240804/코로나 메뉴얼2/covid-manual2.cpp
+++ 240804/코로나 메뉴얼2/covid-manual2.cpp
@@ -4,30 +4,31 @@ using namespace std;
 int main() {
     string arr1[3];
     int arr[3];
-    int arrcount[3]={1, 1, 1};
+    int arrcount[4]={};
     int count=0;
     for(int i=0;i<3;i++){
         cin >> arr1[i] >> arr[i];
         if(arr1[i]=="Y" && arr[i]>=37){
-            count++;
+            arrcount[0]++;
+        }
+        else if(arr1[i]=="N" && arr[i]>=37){
+            arrcount[1]++;
+        }
+        else if(arr1[i]=="Y" && arr[i]<37){
+            arrcount[2]++;
+        }
+        else{
+            arrcount[3]++;
         }
     }
-    if(count>=2){
-        for(int i=0;i<count;i++){
-            arrcount[i]=0;
-        }
-        cout << count << " ";
-        for(int i=0;i<3;i++){
+    if(arrcount[0]>=2){
+        for(int i=0;i<4;i++){
             cout << arrcount[i] << " ";
         }
         cout << "E";
     }
     else{
-        for(int i=2;i>=3-count;i--){
-            arrcount[i]=0;
-         }
-        cout << count << " ";
-        for(int i=0;i<3;i++){
+        for(int i=0;i<4;i++){
             cout << arrcount[i] << " ";
         }
     }
