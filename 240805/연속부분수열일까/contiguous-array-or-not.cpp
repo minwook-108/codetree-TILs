656fca2bece37e22f7efcaf19dd866b59ee1ce9b
minwook-108 <kimmw0321@naver.com>
2024-08-05 22:56:03 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240805/연속부분수열일까/contiguous-array-or-not.cpp
+++ 240805/연속부분수열일까/contiguous-array-or-not.cpp
@@ -3,28 +3,28 @@ using namespace std;
 
 int main() {
     int n1,n2;
-    int A[100]={},B[100]={};
+    string A[100]={},B[100]={};
     cin >> n1 >> n2;
     for(int i=0;i<n1;i++){
         cin >> A[i];
     }
+    string sum="";
     for(int i=0;i<n2;i++){
         cin >> B[i];
+        sum+=B[i];
     }
-    int idx1=0,idx2=n1;
-    bool s=false;
-    for(int i=0;i<n2;i++){
-        for(int j=idx1+1;j<=idx2;j++){
-            if(B[i]==A[j]){
-                idx1=j;
-                idx2=j+1;
-                s=true;
-                break;
-            }
-            s=false;
+    string s="";
+    bool k=false;
+    for(int i=0;i<=n1-n2;i++){
+        for(int j=i;j<i+n2;j++){
+            s+=A[j];
         }
+        if(s==sum){
+            k = true;
+        }
+        s="";
     }
-    if(s==true){
+    if(k==true){
         cout << "Yes";
     }
     else{
