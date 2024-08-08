89aa3fc2debba23ec5612bc4503169712131bc01
minwook-108 <kimmw0321@naver.com>
2024-08-08 23:13:12 +0900

    Title: 중앙값 구하기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240808/중앙값 구하기/find-the-median.py
+++ 240808/중앙값 구하기/find-the-median.py
@@ -3,9 +3,9 @@ a = int(arr[0])
 b = int(arr[1])
 c = int(arr[2])
 
-if a < b and b < c and (a< c or c<a):
+if a < b and b < c:
     print (b)
-elif b < a and a < c and (b <c or c< b):
+elif b < a and a < c:
     print(a)
 else:
     print (c)
