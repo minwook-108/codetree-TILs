729a83eaf1f47f95a8fe97ebe6f1cc129b99cb36
minwook-108 <kimmw0321@naver.com>
2024-08-09 22:46:09 +0900

    Title: 공백을 기준으로 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/공백을 기준으로 출력/output-based-on-space.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a,b;
+    getline(cin,a);
+    getline(cin,b);
+    int cnt1=0,cnt2=0;
+    for(int i=0;i<a.length();i++){
+        if(a[i]==' '){
+            cnt1++;
+            for(int j=i;j<a.length()-1;j++){
+                a[j]=a[j+1];
+            }
+        }
+    }
+    for(int i=0;i<b.length();i++){
+        if(b[i]==' '){
+            cnt2++;
+            for(int j=i;j<b.length()-1;j++){
+                b[j]=b[j+1];
+            }
+        }
+    }
+    for(int i=0;i<a.length()-cnt1;i++){
+        cout << a[i];
+    }
+    for(int i=0;i<b.length()-cnt2;i++){
+        cout << b[i];
+    }
+    return 0;
+}
