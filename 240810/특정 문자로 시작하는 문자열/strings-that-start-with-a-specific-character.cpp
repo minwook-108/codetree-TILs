9be8e1cccbd6a3de7dafed6c76e3fe039e5e797a
minwook-108 <kimmw0321@naver.com>
2024-08-10 21:33:42 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240810/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string arr[20];
+    int n;
+    cin >> n;
+    char a;
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    cin >> a;
+    int cnt=0,sum=0;
+    for(int i=0;i<n;i++){
+        if(a==arr[i][0]){
+            cnt++;
+            sum+=arr[i].length();
+        }
+    }
+    cout << fixed;
+    cout.precision(2);
+    cout << cnt << " " << (double)(sum/cnt);
+    return 0;
+}
