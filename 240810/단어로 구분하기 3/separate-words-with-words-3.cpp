a2b385e5b9d8c6c1591c408f84b9aeeac2b0889c
minwook-108 <kimmw0321@naver.com>
2024-08-10 20:10:57 +0900

    Title: 단어로 구분하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/단어로 구분하기 3/separate-words-with-words-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string arr[10];
+    for(int i=0;i<10;i++){
+        cin >> arr[i];
+    }
+    for(int i=0;i<10;i++){
+        cout << arr[9-i] << endl;
+    }
+    return 0;
+}
