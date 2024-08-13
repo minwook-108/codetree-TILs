3f6ab2c230bb6b06baf0a790b77a76105a78174a
minwook-108 <kimmw0321@naver.com>
2024-08-13 22:54:58 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240813/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void Print10Star(){
+    for(int i=0;i<10;i++){
+        cout << "*";
+    }
+    cout << endl;
+
+}
+int main() {
+    for(int i=0;i<5;i++){
+        Print10Star();
+    }
+    return 0;
+}
