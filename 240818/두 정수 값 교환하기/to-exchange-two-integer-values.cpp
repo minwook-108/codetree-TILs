22a5965a7f2558f6b00697aacefd95486f1a828a
minwook-108 <kimmw0321@naver.com>
2024-08-18 18:48:47 +0900

    Title: 두 정수 값 교환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240818/두 정수 값 교환하기/to-exchange-two-integer-values.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void swap(int &a,int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+    
+}
+int main() {
+    int n,m;
+    cin >> n >> m;
+    swap(n,m);
+    cout << n << " " << m;
+    return 0;
+}
