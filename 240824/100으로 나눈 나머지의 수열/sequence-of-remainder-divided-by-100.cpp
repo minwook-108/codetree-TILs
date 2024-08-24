4b5509400545e4ff94539d618fa9fd8a08338228
minwook-108 <kimmw0321@naver.com>
2024-08-24 22:39:36 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240824/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int Print(int n){
+    if(n==1){
+        return 2;
+    }
+    else if(n==2){
+        return 4;
+    }
+    return Print(n-1)*Print(n-2);
+}
+int main() {
+    int N;
+    cin >> N;
+    cout << Print(N)%100;
+    return 0;
+}
