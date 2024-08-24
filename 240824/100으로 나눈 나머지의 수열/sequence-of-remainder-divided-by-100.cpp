ab2592f80cc8e7c56c0808f3c0ef7de844724f94
minwook-108 <kimmw0321@naver.com>
2024-08-24 22:57:54 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240824/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.cpp
+++ 240824/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.cpp
@@ -7,11 +7,11 @@ int Print(int n){
     else if(n==2){
         return 4;
     }
-    return Print(n-1)*Print(n-2);
+    return (Print(n-1)*Print(n-2))%100;
 }
 int main() {
     int N;
     cin >> N;
-    cout << Print(N)%100;
+    cout << Print(N);
     return 0;
 }
